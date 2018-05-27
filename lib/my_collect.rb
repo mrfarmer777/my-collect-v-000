7308c12b6c9f2702915538c85d35260e0f45c421
Matt Farmer <mrfarmer777@gmail.com>
2018-05-27 18:07:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+#creating my own collect method
+def my_collect(collection)
+  if block_given?
+    res=Array.new
+    i=0
+    while i<collection.size
+      res<<yield(collection[i])
+      i+=1
+    end
+    res
+  else
+    'No block given!'
+  end
+end
