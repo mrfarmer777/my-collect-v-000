19c7327c54a4203561cb90d05b87022429766d63
Matt Farmer <mrfarmer777@gmail.com>
2018-05-27 18:06:51 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
+#creating my own collect method
+def my_collect(collection)
+  if block_given?
+    res=Array.new
+    i=0
+    while i<collection.size 
+      res<<yield(item)
+    end
+    res
+  else
+    'No block given!'
+  end
+end
+  
 
