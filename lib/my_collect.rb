f3c73bc981df40f472133db13d760dda16d0080d
Matt Farmer <mrfarmer777@gmail.com>
2018-05-27 18:07:05 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,13 +3,12 @@ def my_collect(collection)
   if block_given?
     res=Array.new
     i=0
-    while i<collection.size 
+    while i<collection.size
       res<<yield(item)
+      i+=1
     end
     res
   else
     'No block given!'
   end
 end
-  
-
